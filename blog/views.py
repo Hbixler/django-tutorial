b490ff5c9cb64e0fd1d834d6ac248009d2758879
Hailey <hbixler@unomaha.edu>
2021-12-07 15:14:07 -0600

    changed posts again

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(author='hbixler')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
